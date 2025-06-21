17a9446ae5fda0a88b5774605f9962736e66d6e6
Artika-A <artikaafaisal@gmail.com>
2025-06-22 01:04:06 +0800

    Mini Activity

---
+++ section4-4/src/App.jsx
@@ -0,0 +1,15 @@
+import MiniActivity from "./components/MiniActivity"
+
+function App() {
+  
+  return (
+    <div>
+      <h1><u>Mini Activity</u></h1>
+      <MiniActivity/>
+      
+    </div>
+   
+  )
+}
+
+export default App
---
+++ section4-4/src/components/MiniActivity.jsx
@@ -0,0 +1,36 @@
+// Mini Activity: Signup Form
+// Create a form that asks for:
+// 1. Username
+// 2. Email
+
+
+import { useState } from "react";
+
+
+function MiniActivity(){
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+
+    return (
+    <div>
+        <form>
+            <input
+                type="username"
+                placeholder="username"
+                onChange={(e) => setUsername(e.target.value)}
+                />
+            <input
+                type="email"
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+                />
+
+            <h3>Username: {username}</h3> 
+            <h3>Email: {email}</h3>         
+        </form>
+    </div>
+    )
+}
+
+
+export default MiniActivity
